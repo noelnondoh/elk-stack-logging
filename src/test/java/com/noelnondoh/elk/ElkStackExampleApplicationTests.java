39a5329bef73517a1c63221462f0ce5fc169f806
Noel Nondoh <noelnondoh@gmail.com>
2023-09-13 19:55:36 +0000

    init

---
+++ src/test/java/com/noelnondoh/elk/ElkStackExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noelnondoh.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkStackExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
